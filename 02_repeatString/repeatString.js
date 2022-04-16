39f519780130610dfe7de5c61635377643a6a287
chrism-48 <moore4020@gmail.com>
2022-04-16 19:25:57 -0400

    module 1 and 2 passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+let myString;
+const repeatString = function(string, num) {
+    if (num >= 0) {
+      myString = string.repeat(num);
+    } else if (num < 0) {
+        myString = "ERROR"
+    }
+    return myString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
