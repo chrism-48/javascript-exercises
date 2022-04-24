d7615de2c56b7712dc4c02eb75bedfb77998f57d
chrism-48 <moore4020@gmail.com>
2022-04-24 11:02:04 -0400

    Still need to work on this!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
 
-};
+const removeFromArray = function(...args) {  // ---------------- rest operator
+      const array = args[0]; // ------------------------------- get the array
+      return array.filter(val => !args.includes(val));
+      
+}
 
-// Do not edit below this line
-module.exports = removeFromArray;
+
+// // Do not edit below this line
+module.exports = removeFromArray;     
+
+
+
+
