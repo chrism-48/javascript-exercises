b668c3edc32998a6e6790d5ed585a17dcf3e38c6
chrism-48 <moore4020@gmail.com>
2022-04-28 02:56:50 -0400

    stripped logs

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -8,13 +8,11 @@ const sumAll = function(num1, num2) {
     } else if (num1 < 0 ||  num2 < 0) {    
         return 'ERROR';
     } else if (num2 > num1 === true) {
-        console.log('num2 is greater');
         num2 - num1;
         for (let i = num1; i < num2 + 1; i++) {
             mySum = mySum + i; 
         }  
     } else if (num1 > num2 === true) {
-        console.log('num1 is greater');
         num1 - num2;
         for (let j = num2; j < num1 + 1; j++) {
             mySum = mySum + j;           
