f51d6ac7831b5e11666afa0746ccde1ac8c1b081
chrism-48 <moore4020@gmail.com>
2022-04-30 12:11:49 -0400

    all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,24 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {   // fahrenheit to celsius ----------------  [°C] = ([°F] − 32) × 5 ⁄ 9
+  let convertedTemp = (temp - 32) * 5/9;
+  // console.log(Math.round(convertedTemp * 10) / 10);
+  return (Math.round(convertedTemp * 10) / 10);
 };
 
-const ctof = function() {
+const ctof = function(temp) {   // celsius to fahrenheit ----------------  [°F] = [°C] × 9 ⁄ 5 + 32 
+  let convertedTemp = temp * 9/5 + 32;
+  // console.log(Math.round(convertedTemp * 10) / 10);
+  return (Math.round(convertedTemp * 10) / 10);
+};  
 
-};
 
 // Do not edit below this line
 module.exports = {
   ftoc,
   ctof
 };
+
+
+
+
+
+
