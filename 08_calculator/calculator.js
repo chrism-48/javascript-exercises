1fdcc0c5266800b5865ba984216b5f4d05df9dc1
chrism-48 <moore4020@gmail.com>
2022-05-01 10:31:04 -0400

    all tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -4,41 +4,57 @@ const add = function(num1, num2) {
   
 };
 
-const subtract = function() {
-    let subNums = num1 + num2;
+const subtract = function(num1, num2) {
+    let subNums = num1 - num2;
     return subNums;
 };
 
-const sum = function() {
+const sum = function(arr) {
+	let mySum = 0;
+    for (let i = 0; i < arr.length; i++) {       
+        mySum += arr[i];        
+    }
+    return mySum;
+};
+
+const multiply = function(arr) {
+    let myMultiplied = 1;
+    for (let i = 0; i < arr.length; i++) {        
+        myMultiplied *= arr[i] ;      
+    }
+    return myMultiplied;
+};
+
+const power = function(num1, num2) {
+    let myPower = num1 ** num2;
+    return myPower;
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+    if (num < 0) 
+        return -1;
+    else if (num == 0)
+        return 1;
+    else {
+        return (num * factorial(num - 1));
+    }
+   
 };
 
 // Do not edit below this line
-// module.exports = {
-//   add,
-//   subtract,
-//   sum,
-//   multiply,
-//   power,
-//   factorial
-// };
+module.exports = {
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial
+};
 
 
 
 
 
-// add([5,10,20,30]);
-add(3,503);
+
 
